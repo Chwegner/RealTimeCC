f993ca55a0fb280b0356e197c137e2ffbce9af10
ChWegner <chwegner@pm.me>
2019-02-03 20:05:14 +0100

    read DB locations added

---
+++ scripts/db.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var db = /** @class */ (function () {
+    function db() {
+    }
+    return db;
+}());
+exports.db = db;
